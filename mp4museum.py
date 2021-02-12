e4605a634d73e7d4e275da25baef491c4fbf3e1f
JuliusCode <58294183+JuliusCode@users.noreply.github.com>
2021-02-12 12:38:18 +0100

    v5beta3 now with alsa option
    just place an alsa.txt on /boot to select audio output for the player

--- mp4museum.py
+++ mp4museum.py
@@ -1,4 +1,4 @@
-# mp4museum v5 beta 2 - feb 2021
+# mp4museum v5 beta 3 - feb 2021
 
 import time, vlc, os, sys, glob
 
@@ -6,6 +6,13 @@ from subprocess import Popen, PIPE
 import RPi.GPIO as GPIO
 
 FNULL = open(os.devnull, "w")
+
+# read audio device config
+audiodevice = "0"
+
+if os.path.isfile('/boot/alsa.txt'):
+    f = open('/boot/alsa.txt', 'r')
+    audiodevice = f.read(1)
 
 # setup GPIO pin
 GPIO.setmode(GPIO.BOARD)
@@ -19,9 +26,9 @@ def buttonPause(channel):
 def buttonNext(channel):
     player.stop()
 
-# play a video
-def video(source):
-    vlc_instance = vlc.Instance()
+# play media
+def vlc_play(source):
+    vlc_instance = vlc.Instance('-q -A alsa --alsa-audio-device hw:' + audiodevice)
     global player
     player = vlc_instance.media_player_new()
     media = vlc_instance.media_new(source)
@@ -36,7 +43,7 @@ def video(source):
     player.release()
 
 # please do not remove my logo screen
-video("/home/pi/mp4museum5beta.mp4")
+vlc_play("/home/pi/mp4museum5beta.mp4")
 
 # add event listener
 GPIO.add_event_detect(11, GPIO.FALLING, callback = buttonPause, bouncetime = 234)
@@ -45,4 +52,4 @@ GPIO.add_event_detect(13, GPIO.FALLING, callback = buttonNext, bouncetime = 1234
 # the loop
 while(1):
     for files in sorted(glob.glob(r'/media/*/*.*')):
-        video(files)
+        vlc_play(files)
