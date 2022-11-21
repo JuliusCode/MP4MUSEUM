27ae86f9c8c2ee673684ceebeaf71c96599f2407
JuliusCode <58294183+JuliusCode@users.noreply.github.com>
2022-11-21 11:55:10 +0100

    v5.5

--- start.sh
+++ start.sh
@@ -3,4 +3,8 @@
 setterm -cursor off
 sh -c "TERM=linux setterm -foreground black -clear all >/dev/tty0"
 clear
-python /home/pi/mp4museum.py > /dev/null 2>&1
+while true
+do
+	python /home/pi/mp4museum.py > /tmp/mp4museum.log 2>&1
+	clear
+done
